a4025abaab1ad892d14971b9c40294f78dbc7c21
khaifahmi99 <khaifahmi99@github.com>
2022-03-13 23:41:24 +1100

    complete basic level 1 map with player anims

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,24 @@
 import Phaser from 'phaser'
+import { GridEngine } from 'grid-engine'
 
 import HelloWorldScene from './scenes/HelloWorldScene'
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
+	type: Phaser.CANVAS,
+	width: 1400,
+	height: 750,
 	physics: {
 		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
 	},
-	scene: [HelloWorldScene]
+	scene: [HelloWorldScene],
+	plugins: {
+		scene: [{
+			key: 'gridEngine',
+			plugin: GridEngine,
+			mapping: 'gridEngine',
+			start: true,
+		}]
+	}
 }
 
 export default new Phaser.Game(config)
--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -1,39 +1,137 @@
-import Phaser from 'phaser'
+import Phaser, { Types } from 'phaser'
 
-export default class HelloWorldScene extends Phaser.Scene
-{
-	constructor()
-	{
+export default class HelloWorldScene extends Phaser.Scene {
+    private hero!: Types.Physics.Arcade.SpriteWithDynamicBody;
+    private velocity: number = 200;
+    private frameRate: number = 16;
+
+	constructor() {
 		super('hello-world')
 	}
 
-	preload()
-    {
-        this.load.setBaseURL('http://labs.phaser.io')
+    preload() {
+        this.load.image('base_tiles', 'assets/Outside.png');
+        this.load.tilemapTiledJSON('tilemap', 'assets/level_1.json');
 
-        this.load.image('sky', 'assets/skies/space3.png')
-        this.load.image('logo', 'assets/sprites/phaser3-logo.png')
-        this.load.image('red', 'assets/particles/red.png')
+        this.load.spritesheet('hero', 'assets/ash.png', {
+            frameWidth: 32,
+            frameHeight: 48,
+        });
     }
 
-    create()
-    {
-        this.add.image(400, 300, 'sky')
+    create() {
+        const map = this.make.tilemap({ key: 'tilemap' });
+        const tileset = map.addTilesetImage('Outside', 'base_tiles');
 
-        const particles = this.add.particles('red')
+        const ground = map.createLayer('Ground', tileset);
+        const landscape = map.createLayer('Landscape', tileset);
+        const mountain = map.createLayer('mountain', tileset);
+        const lake = map.createLayer('lake', tileset);
+        const grass = map.createLayer('Grass', tileset);
+        const walls = map.createLayer('Wall', tileset);
+        const item = map.createLayer('item', tileset);
+        const buildingLow = map.createLayer('Building Low', tileset);
+        const treeTrunk = map.createLayer('tree trunk', tileset);
+        const signBoards = map.createLayer('Sign', tileset);
+        
+        // Note: Order is importatnt here, hero must be created before high-rise object (tree tops, high buildings)
+        // This will make sure that the high-rise object will cover the hero; looks more realistic
+        this.hero = this.physics.add.sprite(2750, 1800, 'hero').setScale(1);
 
-        const emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        })
+        const treeTop = map.createLayer('tree top', tileset);
+        const buildingHigh = map.createLayer('Building High', tileset);
 
-        const logo = this.physics.add.image(400, 100, 'logo')
+        this.anims.create({
+            key: 'idle_right',
+            frames: this.anims.generateFrameNumbers('hero', { start: 0, end: 5 }),
+            frameRate: this.frameRate * 0.6,
+        });
 
-        logo.setVelocity(100, 200)
-        logo.setBounce(1, 1)
-        logo.setCollideWorldBounds(true)
+        this.anims.create({
+            key: 'idle_up',
+            frames: this.anims.generateFrameNumbers('hero', { start: 6, end: 11 }),
+            frameRate: this.frameRate * 0.6,
+        });
 
-        emitter.startFollow(logo)
+        this.anims.create({
+            key: 'idle_left',
+            frames: this.anims.generateFrameNumbers('hero', { start: 12, end: 17 }),
+            frameRate: this.frameRate * 0.6,
+        });
+
+        this.anims.create({
+            key: 'idle_down',
+            frames: this.anims.generateFrameNumbers('hero', { start: 18, end: 23 }),
+            frameRate: this.frameRate * 0.6,
+        });
+
+
+        this.anims.create({
+            key: 'run_right',
+            frames: this.anims.generateFrameNumbers('hero', { start: 24, end: 29 }),
+            frameRate: this.frameRate,
+        });
+
+        this.anims.create({
+            key: 'run_up',
+            frames: this.anims.generateFrameNumbers('hero', { start: 30, end: 35 }),
+            frameRate: this.frameRate,
+        });
+
+        this.anims.create({
+            key: 'run_left',
+            frames: this.anims.generateFrameNumbers('hero', { start: 36, end: 41 }),
+            frameRate: this.frameRate,
+        });
+
+        this.anims.create({
+            key: 'run_down',
+            frames: this.anims.generateFrameNumbers('hero', { start: 42, end: 47 }),
+            frameRate: this.frameRate,
+        });
+        
+        map.setCollisionBetween(0, 999, true, false, 'tree trunk')
+
+        this.hero.play({ key: 'idle_down', repeat: -1 });
+
+        this.cameras.main.startFollow(this.hero, true);
+        this.cameras.main.setFollowOffset(-this.hero.width, -this.hero.height);
+
+        this.physics.add.collider(this.hero, treeTrunk);
+        this.physics.add.collider(this.hero, buildingLow);
+        this.physics.add.collider(this.hero, mountain);
+        this.physics.add.collider(this.hero, lake);
+        this.physics.add.collider(this.hero, landscape);
+        this.physics.add.collider(this.hero, signBoards);
+        this.physics.add.collider(this.hero, walls);
+
+        treeTrunk.setCollisionBetween(0, 9999);
+        buildingLow.setCollisionBetween(0, 9999);
+        mountain.setCollisionBetween(0, 9999);
+        lake.setCollisionBetween(0, 9999);
+        walls.setCollisionBetween(0, 9999);
+        signBoards.setCollisionBetween(0, 9999);
+
+    }
+
+    update() {
+        const cursors = this.input.keyboard.createCursorKeys();
+
+        if (cursors.left.isDown) {
+            this.hero.setVelocity(-this.velocity, 0);
+            this.hero.play('run_left', true);
+        } else if (cursors.right.isDown) {
+            this.hero.setVelocity(this.velocity, 0);
+            this.hero.play('run_right', true);
+        } else if (cursors.up.isDown) {
+            this.hero.setVelocity(0, -this.velocity);
+            this.hero.play('run_up', true);
+        } else if (cursors.down.isDown) {
+            this.hero.setVelocity(0, this.velocity);
+            this.hero.play('run_down', true);
+        } else {
+            this.hero.setVelocity(0, 0);
+            this.hero.play('idle_down', true);
+        }
     }
 }
